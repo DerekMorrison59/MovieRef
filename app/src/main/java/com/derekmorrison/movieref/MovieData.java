99b305f75e1815be5155b1554eb90cdcd326f90d
Derek Morrison <DerekMorrison59@gmail.com>
2015-11-01 10:13:17 -0700

    Additional comments.

--- app/src/main/java/com/derekmorrison/movieref/MovieData.java
+++ app/src/main/java/com/derekmorrison/movieref/MovieData.java
@@ -3,14 +3,12 @@ package com.derekmorrison.movieref;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-/**
+/*
  * Created by Derek on 10/17/2015.
- *
  *
  * The addition of 'Parcelable' was largely taken from: http://shri.blog.kraya.co.uk/
  *
  * http://shri.blog.kraya.co.uk/2010/04/26/android-parcel-data-to-pass-between-activities-using-parcelable-classes/
- *
  */
 public class MovieData implements Parcelable {
 
@@ -28,10 +26,8 @@ public class MovieData implements Parcelable {
         this.setmPosterPath(mPosterPath);
     }
 
-    /**
-     *
-     * Constructor to use when re-constructing object
-     * from a parcel
+    /*
+     * Constructor to use when re-constructing object from a parcel
      *
      * @param in a parcel from which to read this object
      */
@@ -42,9 +38,7 @@ public class MovieData implements Parcelable {
     @Override
     public void writeToParcel(Parcel dest, int flags) {
 
-        // We just need to write each field into the
-        // parcel. When we read from parcel, they
-        // will come back in the same order
+        // Write each field into the parcel in a specific order
         dest.writeString(getmTitle());
         dest.writeString(getmReleaseDate());
         dest.writeString(getmPosterPath());
@@ -52,18 +46,14 @@ public class MovieData implements Parcelable {
         dest.writeString(getmOverview());
     }
 
-    /**
-     *
-     * Called from the constructor to create this
-     * object from a parcel.
+    /*
+     * Called from the constructor to create this object from a parcel.
      *
      * @param in parcel from which to re-create object
      */
     private void readFromParcel(Parcel in) {
 
-        // We just need to read back each
-        // field in the order that it was
-        // written to the parcel
+        // Read back each field in the order that it was written to the parcel
         setmTitle(in.readString());
         setmReleaseDate(in.readString());
         setmPosterPath(in.readString());
@@ -76,8 +66,7 @@ public class MovieData implements Parcelable {
         return 0;
     }
 
-    /**
-     *
+    /*
      * This field is needed for Android to be able to
      * create new objects, individually or as arrays.
      *
@@ -85,26 +74,22 @@ public class MovieData implements Parcelable {
      * constructor to create the object and use another
      * method to hyrdate it as necessary.
      *
-     * I just find it easier to use the constructor.
-     * It makes sense for the way my brain thinks ;-)
-     *
      */
     public static final Parcelable.Creator CREATOR =
-            new Parcelable.Creator() {
-                public MovieData createFromParcel(Parcel in) {
-                    return new MovieData(in);
-                }
+        new Parcelable.Creator() {
+            public MovieData createFromParcel(Parcel in) {
+                return new MovieData(in);
+            }
 
-                public MovieData[] newArray(int size) {
-                    return new MovieData[size];
-                }
-            };
+            public MovieData[] newArray(int size) {
+                return new MovieData[size];
+            }
+        };
 
-
+    // getters and setters for the data members
     public String getmTitle() {
         return mTitle;
     }
-
     public void setmTitle(String mTitle) {
         this.mTitle = mTitle;
     }
@@ -112,7 +97,6 @@ public class MovieData implements Parcelable {
     public String getmReleaseDate() {
         return mReleaseDate;
     }
-
     public void setmReleaseDate(String mReleaseDate) {
         this.mReleaseDate = mReleaseDate;
     }
@@ -120,7 +104,6 @@ public class MovieData implements Parcelable {
     public String getmPosterPath() {
         return mPosterPath;
     }
-
     public void setmPosterPath(String mPosterPath) {
         this.mPosterPath = mPosterPath;
     }
@@ -128,7 +111,6 @@ public class MovieData implements Parcelable {
     public String getmRating() {
         return mRating;
     }
-
     public void setmRating(String mRating) {
         this.mRating = mRating;
     }
@@ -136,7 +118,6 @@ public class MovieData implements Parcelable {
     public String getmOverview() {
         return mOverview;
     }
-
     public void setmOverview(String mOverview) {
         this.mOverview = mOverview;
     }
